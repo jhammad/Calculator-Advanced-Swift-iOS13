7c9a1ea703c779c4298fe8d9b40eb79e477bfe3b
jhammad <javier.hammad@gmail.com>
2024-01-16 10:57:50 +0000

    Use computer property to refract the code

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,31 +6,42 @@ class ViewController: UIViewController {
     
     private var isFinishedTypingNumber: Bool = true
     
-    
+    // Define a computed property named displayValue of type Double
+    private var displayValue: Double {
+        // Getter for the displayValue property
+        get {
+            // Use guard to safely unwrap and convert displayLabel.text to a Double
+            guard let number = Double(displayLabel.text!) else {
+                // If the conversion fails, trigger a fatal error with an informative message
+                fatalError("Cannot convert display label text to a Double")
+            }
+            // Return the successfully converted number
+            return number
+        }
+        // Setter for the displayValue property
+        set {
+            // Update displayLabel.text with the string representation of the new value
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
-        // make sure that is always a number on display
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert display label text to a Double")
-        }
         if let calcMethod = sender.currentTitle {
             if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
+                displayValue *= -1
             }
             else if calcMethod == "AC" {
                 displayLabel.text = "0"
             }
             else if calcMethod == "%" {
-                displayLabel.text = String(number * 0.01)
+                displayValue *= 0.01
             }
         }
     }
     
-    
-    
-    
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        
         // Check if the sender has a current title (text on a button, for example)
         if let numValue = sender.currentTitle {
             // Check if the user has finished typing a number
@@ -44,20 +55,15 @@ class ViewController: UIViewController {
                 
                 // Check if the button pressed corresponds to the decimal point (".")
                 if numValue == "." {
-                    // Try to convert the current display label text to a Double
-                    guard let currentDisplayValue = Double(displayLabel.text!) else {
-                        // If conversion fails, print an error message and terminate the program
-                        fatalError("Cannot convert display label text to a Double")
-                    }
                     // Check if the current display value is an integer (has no decimal part)
-                    let isInt = floor(currentDisplayValue) == Double(displayLabel.text!)
+                    let isInt = floor(displayValue) == displayValue
                     // If the current value is not an integer (has a decimal part), return from the function
                     //to avoid adding decimals
                     if !isInt {
                         return
                     }
                 }
-
+                
                 // If the user hasn't finished typing a number, append the current button's title to the display label
                 displayLabel.text = displayLabel.text! + numValue
             }
